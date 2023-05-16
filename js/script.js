10cf7121b2c24ae73cbce99a8116a53fcecfea8b
AntonelloStefania <antonelloste94@gmail.com>
2023-05-16 17:44:31 +0200

    aggiunta costante numerica

--- js/script.js
+++ js/script.js
@@ -7,8 +7,11 @@ let cognome = prompt ('Inserisci il tuo cognome');
 //prompt colore preferito
 let colore_preferito = prompt ('Inserisci il tuo colore preferito');
 
+//costante numerica
+const num21 ='21'
+
 //generazione password
-let psw_gen = `${nome}${cognome}${colore_preferito}21`
+let psw_gen = `${nome}${cognome}${colore_preferito}${num21}`
 
 //output password generata a schermo
 document.getElementById('psw_gen').innerHTML = psw_gen
